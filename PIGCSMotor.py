c2127007c62ab4c40efda56f34cba25bf779ff9c
Michael Schneider <mschneid@mbi-berlin.de>
2022-02-01 16:56:30 +0100

    single file for ctrl and axis classes

--- PIGCSMotor.py
+++ PIGCSMotor.py
@@ -9,7 +9,7 @@ PI Hexapod tango device server
 from pipython import GCS2Device
 import tango
 from tango import AttrWriteType, DevState, DevDouble, DevBoolean, DeviceProxy
-from tango.server import Device, attribute, command, device_property
+from tango.server import Device, attribute, command, device_property, run
 import sys
 import time
 
@@ -75,8 +75,8 @@ class PIGCSController(Device):
         doc_out="Result code (0: moving)",
     )
     def set_position(self, position):
-        axis = position[0]
-        target = float(position[1])
+        axis, target = position.split("=")
+        target = float(target)
         self.ctrl.MOV(axis, target)
         error = self.ctrl.qERR()
         return error
@@ -124,9 +124,85 @@ class PIGCSController(Device):
 
     @command
     def halt(self):
-        """Halt motion smoothly."""
+        """Halt motion smoothly. Doesn't stop reference moves."""
         self.ctrl.HLT(noraise=True)
+
+    @command
+    def stop(self):
+        """Abruptly stop all motions, including reference moves."""
+        self.ctrl.STP(noraise=True)
+
+
+class PIGCSAxis(Device):
+    """"""
+
+    controller = device_property(
+        dtype=str,
+        default_value="",
+        doc="tango fqdn of controller device server",
+    )
+
+    axis = device_property(
+        dtype=str,
+        default_value="",
+        doc="name of axis on controller (e.g., X, Y, Z, U, V, W)",
+    )
+
+    position = attribute(
+        dtype=float,
+        access=AttrWriteType.READ_WRITE,
+    )
+
+    limit_switch = attribute(
+        dtype=int,
+        access=AttrWriteType.READ,
+    )
+
+    def init_device(self):
+        super(PIGCSAxis, self).init_device()
+        self.ctrl = DeviceProxy(self.controller)
+        ctrl_axes = self.ctrl.get_axis_names()
+        if self.axis in ctrl_axes:
+            self.set_state(DevState.ON)
+            self._position = 0
+            self._limit = 0
+        else:
+            print(f"Axis {self.axis} not in {ctrl_axes}", file=self.log_error)
+            self.set_state(DevState.FAULT)
+
+    def always_executed_hook(self):
+        state = self.ctrl.query_axis_state(self.axis)
+        print(f"READ STATE: {self.axis} {state}")
+        self._position = state[0]
+        self._limit = bool(state[1])
+        if state[2]:
+            self.set_state(DevState.MOVING)
+        else:
+            self.set_state(DevState.ON)
+
+    def read_position(self):
+        return self._position
+
+    def write_position(self, position):
+        ans = self.ctrl.set_position(f"{self.axis}={position}")
+        print(f"SET POS: {self.axis} -> {position} (ans={ans})")
+        if ans == 0:
+            self.set_state(DevState.MOVING)
+
+    def read_limit_switch(self):
+        return self._limit
+
+    @command
+    def halt_axis(self):
+        """Smoothly stop motion"""
+        self.ctrl.halt()
+
+    @command
+    def stop_axis(self):
+        """Abruptly stop all motion."""
+        self.ctrl.stop()
 
 
 if __name__ == "__main__":
-    PIGCSController.run_server()
+    # PIGCSController.run_server()
+    run((PIGCSController, PIGCSAxis))
